60743c2063d918b28f8b6c7eba8d4f46f8771dcc
Tor Nordqvist <tornord@gmail.com>
2021-09-04 10:15:35 +0200

    removed lib from git

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import numeric from "numeric";
-import seedrandom from "seedrandom";
-import fmin from "fmin";
-export * from "./FormatFunctions";
-export * from "./Math";
-export * from "./quadprog";
-export * from "./RandomNumberGenerator";
-export { fmin, numeric, seedrandom };
