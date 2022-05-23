815d2a8479eb90fe19501f01e0ce832867dd5f71
Tor Nordqvist <tornord@gmail.com>
2022-05-23 12:57:48 +0200

    Periodic & contrained cubic spline

--- src/interpolation.ts
+++ src/interpolation.ts
@@ -1,6 +1,7 @@
+import { round, numeric } from "../src";
 import { indexOf } from ".";
 
-const { sqrt, hypot } = Math;
+const { hypot, pow, floor, max } = Math;
 
 export class Node {
   constructor(public x: number, public y: number) {}
@@ -80,6 +81,7 @@ export class CubicHermite extends Curve {
     return y1 + d * (y2 - y1) + d * (1 - d) * g + d * d * (1 - d) * c;
   }
 }
+
 export class NaturalCubicSpline extends Curve {
   constructor(xs: number[], ys: number[]) {
     super(xs, ys);
@@ -92,24 +94,26 @@ export class NaturalCubicSpline extends Curve {
 
   calcualate() {
     const n = this.xs.length;
-    const hs = [];
-    const bs = [];
+    const hs = new Array(n - 1);
+    const bs = new Array(n - 1);
     for (let i = 0; i < n - 1; i++) {
       const h = this.xs[i + 1] - this.xs[i];
-      hs.push(h);
-      bs.push((6 * (this.ys[i + 1] - this.ys[i])) / h);
+      hs[i] = h;
+      bs[i] = (6 * (this.ys[i + 1] - this.ys[i])) / h;
     }
-    const us = [];
-    const vs = [];
-    us.push(Number.NaN);
-    us.push(2 * (hs[0] + hs[1]));
-    vs.push(Number.NaN);
-    vs.push(bs[1] - bs[0]);
+    const us = new Array(n - 1);
+    const vs = new Array(n - 1);
+    us[0] = 0;
+    us[1] = 2 * (hs[0] + hs[1]);
+    vs[0] = 0;
+    vs[1] = bs[1] - bs[0];
     for (let i = 2; i < n - 1; i++) {
-      us.push(2 * (hs[i] + hs[i - 1]) - Math.pow(hs[i - 1], 2) / us[i - 1]);
-      vs.push(bs[i] - bs[i - 1] - (hs[i - 1] * vs[i - 1]) / us[i - 1]);
+      us[i] = 2 * (hs[i] + hs[i - 1]) - pow(hs[i - 1], 2) / us[i - 1];
+      vs[i] = bs[i] - bs[i - 1] - (hs[i - 1] * vs[i - 1]) / us[i - 1];
     }
-    const zs = this.xs.map(() => 0);
+    const zs = new Array(n);
+    zs[0] = 0;
+    zs[n - 1] = 0;
     for (let i = n - 2; i > 0; i--) {
       zs[i] = (vs[i] - hs[i] * zs[i + 1]) / us[i];
     }
@@ -135,6 +139,198 @@ export class NaturalCubicSpline extends Curve {
       return this.ys[n - 1];
     }
     const x0 = this.xs[idx];
+    const t = x - x0;
+    const y0 = this.ys[idx];
+    return y0 + t * (this.c[idx] + t * (this.b[idx] + t * this.a[idx]));
+  }
+}
+
+function mat2str(x: any) {
+  const fourDec = (d: number) => round(d, 4);
+  const vec2str = (vs: number[]) => `[${vs.map(fourDec).join(", ")}]`;
+  if (!Array.isArray(x)) {
+    return String(fourDec(x));
+  }
+  if (x.length > 0 && Array.isArray(x[0])) {
+    return `[${x.map(vec2str).join(",\n")}]`;
+  }
+  return vec2str(x);
+}
+
+function addAbcEquation(
+  aMat: number[][],
+  bVec: number[],
+  colStartIndex: number,
+  rowIndex: number,
+  coeffs: number[],
+  b: number
+) {
+  const n = bVec.length / 3;
+  bVec[rowIndex] = b;
+  const ar = new Array(3 * n);
+  for (let j = 0; j < n; j++) {
+    for (let k = 0; k < 3; k++) {
+      let a = 0;
+      if (j === colStartIndex) {
+        a = coeffs[k];
+      }
+      ar[3 * j + k] = a;
+    }
+  }
+  aMat[rowIndex] = ar;
+}
+
+function addAbcEquations(
+  aMat: number[][],
+  bVec: number[],
+  startIndex: number,
+  currCoeffs: number[],
+  nextCoeffs: number[],
+  bs: number[],
+  loop: boolean = true
+) {
+  const n = bVec.length / 3;
+  for (let i = 0; i < n - (loop ? 0 : 1); i++) {
+    bVec[i + startIndex] = bs[i];
+    const ar = new Array(3 * n);
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < 3; k++) {
+        let a = 0;
+        if (j === i) {
+          a = currCoeffs[k];
+        } else if (j === (i + 1) % n) {
+          a = nextCoeffs[k];
+        }
+        ar[3 * j + k] = a;
+      }
+    }
+    aMat[i + startIndex] = ar;
+  }
+}
+
+// Periodic cubic spline, where start matches end.
+// x-values are equal to index, i.e x0=0, x1=1 etc.
+export class PeriodicCubicSpline {
+  constructor(ys: number[]) {
+    this.ys = ys;
+    this.calcualate();
+  }
+
+  ys: number[];
+
+  a: number[];
+  b: number[];
+  c: number[];
+
+  calcualate() {
+    const n = this.ys.length;
+    const bs = new Array(3 * n);
+    const aa = new Array(3 * n);
+    const zs = [...Array(n)].map(() => 0);
+    const ds = this.ys.map((d, i, a) => a[(i + 1) % n] - d);
+
+    addAbcEquations(aa, bs, 0, [1, 1, 1], [0, 0, 0], ds);
+    addAbcEquations(aa, bs, n, [3, 2, 1], [0, 0, -1], zs);
+    addAbcEquations(aa, bs, 2 * n, [6, 2, 0], [0, -2, 0], zs);
+    const res = numeric.solve(aa, bs);
+    this.a = new Array(n - 1);
+    this.b = new Array(n - 1);
+    this.c = new Array(n - 1);
+    for (let i = 0; i < n; i++) {
+      this.a[i] = res[3 * i];
+      this.b[i] = res[3 * i + 1];
+      this.c[i] = res[3 * i + 2];
+    }
+    // console.log(mat2str(aa), mat2str(bs), this.a, this.b, this.c);
+  }
+  y(x: number) {
+    const n = this.ys.length;
+    if (n === 0) {
+      return Number.NaN;
+    }
+    if (n === 1) {
+      return this.ys[0];
+    }
+    const xm = x - floor(x / n) * n;
+    const idx = floor(xm);
+    const x0 = idx;
+    const t = xm - x0;
+    const y0 = this.ys[idx];
+    return y0 + t * (this.c[idx] + t * (this.b[idx] + t * this.a[idx]));
+  }
+}
+
+// Calculates constrained variants of natural cubic spline.
+// 1) With known first derivative in start and/or end.
+// 2) With symmetric (first/last interval is mirrored) start and/or end (derivative=0)
+// x-values are equal to index, i.e x0=0, x1=1 etc.
+export class ConstrainedCubicSpline {
+  constructor(ys: number[], startDerivative: number | null = null, endDerivative: number | null = null) {
+    this.ys = ys;
+    this.startDerivative = startDerivative;
+    this.endDerivative = endDerivative;
+    this.calcualate();
+  }
+
+  ys: number[];
+  startDerivative: number | null;
+  endDerivative: number | null;
+
+  a: number[];
+  b: number[];
+  c: number[];
+
+  calcualate() {
+    const n = this.ys.length - 1;
+    const bs = new Array(3 * n);
+    const aa = new Array(3 * n);
+    const ds = new Array(n - 1);
+    for (let i = 0; i < n; i++) {
+      ds[i] = this.ys[i + 1] - this.ys[i];
+    }
+    const zs = ds.map(() => 0);
+
+    addAbcEquations(aa, bs, 0, [1, 1, 1], [0, 0, 0], ds, true);
+    addAbcEquations(aa, bs, n, [3, 2, 1], [0, 0, -1], zs, false);
+    addAbcEquations(aa, bs, 2 * n - 1, [6, 2, 0], [0, -2, 0], zs, false);
+    if (this.startDerivative === null) {
+      addAbcEquation(aa, bs, 0, 3 * n - 2, [0, 2, 0], 0); // Natural constraint (second derivative=0)
+    } else {
+      addAbcEquation(aa, bs, 0, 3 * n - 2, [0, 0, 1], this.startDerivative);
+    }
+    if (this.endDerivative === null) {
+      addAbcEquation(aa, bs, n - 1, 3 * n - 1, [6, 2, 0], 0); // Natural constraint (second derivative=0)
+    } else {
+      addAbcEquation(aa, bs, n - 1, 3 * n - 1, [3,2,1], this.endDerivative);
+    }
+    const res = numeric.solve(aa, bs);
+    this.a = new Array(n - 1);
+    this.b = new Array(n - 1);
+    this.c = new Array(n - 1);
+    for (let i = 0; i < n; i++) {
+      this.a[i] = res[3 * i];
+      this.b[i] = res[3 * i + 1];
+      this.c[i] = res[3 * i + 2];
+    }
+    // console.log(mat2str(aa), mat2str(bs), this.a, this.b, this.c);
+  }
+  y(x: number) {
+    const n = this.ys.length;
+    if (n === 0) {
+      return Number.NaN;
+    }
+    if (n === 1) {
+      return this.ys[0];
+    }
+    let idx;
+    if (x >= n - 1) {
+      idx = n - 2;
+    } else if (x < 0) {
+      idx = 0;
+    } else {
+      idx = floor(x);
+    }
+    const x0 = idx;
     const t = x - x0;
     const y0 = this.ys[idx];
     return y0 + t * (this.c[idx] + t * (this.b[idx] + t * this.a[idx]));
--- test/interpolation.test.ts
+++ test/interpolation.test.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai";
 
-import { CubicHermite, Linear, NaturalCubicSpline } from "../src";
+import { CubicHermite, Linear, NaturalCubicSpline, PeriodicCubicSpline, ConstrainedCubicSpline, round } from "../src";
+
+const round4 = (x: number) => round(x, 4);
 
 describe("interpolation", () => {
   it("Linear", () => {
@@ -34,5 +36,63 @@ describe("interpolation", () => {
     expect(lin.y(4.5)).to.equal(10.5);
     expect(lin.y(5)).to.equal(12);
     expect(lin.y(6)).to.equal(12);
+  });
+  it("PeriodicCubicSpline", () => {
+    const lin1 = new PeriodicCubicSpline([3, 6, 6, 5]);
+    expect(lin1.y(0)).to.equal(3);
+    expect(lin1.y(1)).to.equal(6);
+    expect(lin1.y(2)).to.equal(6);
+    expect(lin1.y(3)).to.equal(5);
+    expect(lin1.y(-1)).to.equal(5);
+    expect(lin1.y(4)).to.equal(3);
+    expect(round4(lin1.y(0.5))).to.equal(4.3125);
+    expect(round4(lin1.y(0.1))).to.equal(3.1245);
+    expect(round4(lin1.y(3.9))).to.equal(2.975);
+    expect(round4(lin1.y(1.1))).to.equal(6.189);
+    const lin2 = new PeriodicCubicSpline([1, 2, 3, 4, 5]);
+    expect(round4(lin2.y(4.1))).to.equal(4.7309);
+    expect(round4(lin2.y(4.5))).to.equal(3);
+    expect(round4(lin2.y(4.9))).to.equal(1.2691);
+    expect(round4(lin2.y(4.99))).to.equal(1.0224);
+    expect(round4(lin2.y(-0.01))).to.equal(1.0224);
+    const lin3 = new PeriodicCubicSpline([1]);
+    expect(lin3.y(0)).to.equal(1);
+  });
+  it("ConstrainedCubicSpline, natural", () => {
+    const lin1 = new NaturalCubicSpline([0, 1, 2, 3], [3, 6, 6, 5]);
+    const lin2 = new ConstrainedCubicSpline([3, 6, 6, 5]);
+    expect(round4(lin1.y(0.7))).to.equal(round4(lin2.y(0.7)));
+    expect(round4(lin1.y(1.2))).to.equal(round4(lin2.y(1.2)));
+    expect(round4(lin1.y(2.6))).to.equal(round4(lin2.y(2.6)));
+    expect(round4(lin1.y(0.0))).to.equal(round4(lin2.y(0.0)));
+    expect(round4(lin1.y(3.0))).to.equal(round4(lin2.y(3.0)));
+    expect(round4(lin2.y(3.5))).to.equal(4.475);
+    expect(round4(lin2.y(-0.5))).to.equal(1.225);
+  });
+  it("ConstrainedCubicSpline, start derivative", () => {
+    const lin1 = new ConstrainedCubicSpline([1,3,2,2,1], -0.5);
+    const lin2 = new ConstrainedCubicSpline([1,3,2,2,1]);
+    expect(round4(lin1.y(1))).to.equal(3);
+    expect(round4(lin1.y(0))).to.equal(1);
+    expect(round4(lin1.y(0.1))).to.equal(1.0054);
+    expect(round4(lin2.y(0.1))).to.equal(1.2884);
+  });
+  it("ConstrainedCubicSpline, end derivative", () => {
+    const lin1 = new ConstrainedCubicSpline([1,3,2,2,1], null, 1.0);
+    const lin2 = new ConstrainedCubicSpline([1,3,2,2,1]);
+    expect(round4(lin1.y(3))).to.equal(2);
+    expect(round4(lin1.y(4))).to.equal(1);
+    expect(round4(lin1.y(3.9))).to.equal(0.9393);
+    expect(round4(lin2.y(3.9))).to.equal(1.1389);
+  });
+  it("ConstrainedCubicSpline, symmetric start&end (first der. = 0)", () => {
+    const lin1 = new ConstrainedCubicSpline([1,3,3,1], 0, 0);
+    const lin2 = new ConstrainedCubicSpline([1,3,3,1]);
+    expect(round4(lin1.y(1))).to.equal(3);
+    expect(round4(lin1.y(3))).to.equal(1);
+    expect(round4(lin1.y(0.1))).to.equal(1.038);
+    expect(round4(lin2.y(0.1))).to.equal(1.2396);
+    expect(round4(lin1.y(2.9))).to.equal(1.038);
+    expect(round4(lin2.y(2.9))).to.equal(1.2396);
   });
 });
